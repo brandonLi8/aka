9445979689a6b04e48ffbe5cda4e96ae188acddd
Brandon Li <brandon.li820@icloud.com>
2021-08-10 00:28:43 -0600

    add CLI

---
+++ bin/index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+// Copyright © 2021 Brandon Li. All rights reserved.
+
+/**
+ * The CLI for AKA.
+ *
+ * @author Brandon Li <brandon.li@berkeley.edu>
+ */
+
+import { program } from 'commander';
+import { start } from '../lib/index.js';
+import forever from 'forever';
+import path from 'path';
+
+program
+  .command('dev')
+  .description('run the localhost once (not in the background)')
+  .action(start);
+
+program
+  .command('start')
+  .description('run the aka server continuously in the background.')
+  .action(() => {
+    forever.startDaemon(path.resolve('./lib/index.js'), {
+      uid: 'aka'
+    });
+
+    console.log('Running in background process. Visit aka/ in your browser');
+  });
+
+program
+  .command('stop')
+  .description('run the aka server continuously in the background.')
+  .action(() => {
+    forever.stop(path.resolve('./lib/index.js'), true);
+
+    console.log('Stopped.');
+  });
+
+program.parse(process.argv)
