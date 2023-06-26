c73aaf6b169504ef0282fd9269175790efb872fe
Brandon Li <brandon.li820@icloud.com>
2023-06-25 22:28:28 -0700

    ascii art

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,6 @@
  */
 
 // modules
-process.env.TEST = 3;
 import forever from 'forever';
 import mkdirp from 'mkdirp';
 import os from 'os';
@@ -21,45 +20,47 @@ import { start } from '../lib/index.js';
 
 // constants
 const DEFAULT_DATA_DIR = path.join(process.env.HOME || os.homedir(), './.aka');
-
-program
-  .command('dev')
-  .description('run aka once (not in the background)')
-  .option('-d, --data-dir <path>', 'path for storing the application data (default: $HOME/.aka/data)')
-  .action(({ dataDir }) => {
-    process.env.DATA_DIR = process.env.DATA_DIR || dataDir || DEFAULT_DATA_DIR;
-    start();
-  });
+const AKA = String.raw`__
+_____  |  | _______
+\__  \ |  |/ /\__  \
+ / __ \|    <  / __ \_
+(____  /__|_ \(____  /
+     \/     \/     \/
+`;
 
 program
   .command('start')
   .description('run the aka server continuously in the background.')
   .option('-d, --data-dir <path>', 'path for storing the application data (default: $HOME/.aka/data)')
-  .action(async ({ dataDir }) => {
+  .option('-C, --no-continuous', 'If provided, the server wont run continuously in the background')
+  .action(async ({ dataDir, continuous }) => {
+    process.env.DATA_DIR = process.env.DATA_DIR || dataDir || DEFAULT_DATA_DIR;
 
     // Ensure that the server isn't already running.
     if ((await util.promisify(forever.list)(false) || []).find(process => process.uid === 'aka')) {
       return console.error('aka is already running. Use `aka stop` to stop it.');
     }
 
-    process.env.DATA_DIR = process.env.DATA_DIR || dataDir || DEFAULT_DATA_DIR;
+    if (!continuous) {
+      return start();
+    }
 
     const logDir = path.join(process.env.DATA_DIR, './log');
     await mkdirp.sync(logDir);
 
-    forever.startDaemon(fileURLToPath(new URL('./lib/index.js', import.meta.url)), {
+    forever.startDaemon(fileURLToPath(new URL('../lib/index.js', import.meta.url)), {
       uid: 'aka',
       outFile: path.join(logDir, 'aka.out'),
       errFile: path.join(logDir, 'aka.err'),
       logFile: path.join(logDir, 'aka.log')
     });
 
-    console.log('Running in background process. Visit aka/ in your browser');
+    console.log(AKA + '\nThank you for trying AKA! Visit aka/admin in your browser. ');
   });
 
 program
   .command('stop')
-  .description('run the aka server continuously in the background.')
+  .description('stop the aka server')
   .action(async () => {
 
     // Ensure that the server is running.
